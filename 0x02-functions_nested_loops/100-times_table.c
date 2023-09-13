d9dfbae5e72c47907996ca5bff9baa80f21788f0
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-09-13 07:09:38 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,46 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - function to sum multiples of 3or 5 in numbers below 1024
+ * print_times_table - prints the times table of the inputs
+ * starting: with 0
+ * @n: The printed values of the time table
  * Return: 0
  */
 
+void print_times_table(int n)
+
+{
+	int num, mult, prod;
+
+	if (n = 0 && <= 15)
+	{
+		for  (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+				for (mult = 1; mult <= n; mult++)
+				{
+					_putchar(',');
+					_putchar(' ');
+					prod = num * mult;
+					if (prod <= 99)
+						_putchar(' ');
+					if (prod <= 9)
+						_putchar(' ');
+					if (prod >= 100)
+					{
+						_putchar((prod / 100) + '0');
+						_putchar(((prod / 10)) % 10 + '0');
+					}
+					else  if (prod <= 99 && prod >= 10)
+					{
+						_putchar((prod / 10) + '0');
+					}
+					_putchar((prod % 10) + '0');
+				}
+			_putchar('\n');
+		}
+	}
 }
+
+
+
