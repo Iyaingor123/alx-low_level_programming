f6f0bbff112fd7a31e3591c3746d062bd3b2fa8c
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-09-14 05:09:57 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function that multiple variables
+ * @a: a - variable
+ * @b: b - variable
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
