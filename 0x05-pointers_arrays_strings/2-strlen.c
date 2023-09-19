560089d0727c04444070c48a5c25d416d42a24ca
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-09-18 23:45:56 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,19 @@
 
 /**
  * _strlen - functions that return the length of a string
- * @str:the length of the string
+ * @s: the length of the string
  * Return: the string length
  */
 
-size_t_strlen(const char *str)
+int _strlen(char *s)
 
 {
-size_t length = 0
+int longt = 0;
 
-while (*str++)
-length++;
-return (length);
+while (*s != '\0')
+{
+longt++;
+s++;
+}
+return (longt);
 }
