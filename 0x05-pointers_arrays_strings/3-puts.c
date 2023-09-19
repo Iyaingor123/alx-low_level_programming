575143d1e6888d781159ec7267f04eef44523e46
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-09-18 23:59:22 -0700

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "mani.h"
+
+/**
+ * _puts - puts a string following a new line to stdout
+ * @str: print string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar (*str++)
+			_putchar ('\n')
+	}
+}
