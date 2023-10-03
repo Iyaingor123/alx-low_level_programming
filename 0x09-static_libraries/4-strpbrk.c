80d9acadc08129b8aedc28271bce64df94232abc
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-10-02 19:51:51 -0700

    static libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function entry point
+ * @s: value input
+ * @accept: value input
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
