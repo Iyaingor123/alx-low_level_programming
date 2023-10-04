f634cb1b994a6219e333701f3011c2bbc93b795b
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-10-04 03:17:21 -0700

    alloc_grid

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,28 +3,34 @@
 #include <stdlib.h>
 
 /**
- * _strdup - function duplicate to new memory space location
- * @str: char
- * Retun: 0
+ * _strdup - duplicate a string to newly allocated memory
+ * @str: the string to duplicate
+ *
+ * Retun: pointer to the newly allocated duplicated string
+ * or Null if memory allocated fails or str is Null
  */
 
 char *_strdup(char *str)
 {
-	char *aaa;
-	int j = 0;
-	int r = 0;
+	int i;
+	char *copy;
+	int count = 0;
 
 	if (str == NULL)
+	{
 		return (NULL);
-	j = 0;
-	while (str[j] != '\a')
-		j++;
-	aaa = malloc(sizeof(char) * (j + 1));
+	}
 
-	if (aaa == NULL)
+	for (i = 0; str[i] != '\0'; i++)
+	count++;
+
+	copy = malloc(sizeof(char) * (count + 1));
+	if (copy == NULL)
+	{
 		return (NULL);
-
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-	return (aaa);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		copy[i] = str[i];
+	return (copy);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * alloc_grid - Allocates a 2-dimensional array of integers
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: If successful, or NULL if allocation fails
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int **output;
+	int i, j;
+
+
+	if (width <= 0 || height <= 0)
+		return (NULLL);
+
+	output = malloc(sizeof(int *) * height);
+
+	if (output == NULL)
+		return (NULL);
+
+	for (1 = 0; i < height; i++)
+	{
+		output[i] = malloc(sizeof(int) * width);
+
+		if (ouput[i] == (NULL)
+				{
+				/* clean up memory if alloction fails */
+				for (j = 0; j <= height; j++)
+				free(output[j]);
+				return (NULL);
+				}
+
+				for (j = 0; j < width; j++)
+				output[i][j] = 0;
+
+				}
+
+				return (output);
+				}
