483398869d22a4edb26a60ec6b88164d3942ffc6
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-10-04 15:18:44 -0700

    args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,53 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * argstostr - convert the params passed to the program to string
- * @ac: the arguement count
- * @av: the arguement vector
- *
- * Return: ..
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
-
 char *argstostr(int ac, char **av)
 {
-int ch = 0;
-int i = 0;
-int j = 0;
-int k = 0;
-char *s;
+	int i, n, r = 0, l = 0;
+	char *str;
 
-if (ac == 0 || av == NULL)
-return (NULL);
-while (i < ac)
-{
-while (av[i][j])
-{
-ch++;
-j++;
-}
-j = 0;
-i++;
-}
-s = malloc((sizeof(char) * ch) +ac + 1);
-i = 0;
-while (av[i])
-{
-while
-(av[i][j]);
-{
-s[k] = av[i][j];
-k++;
-j++;
-}
-s[k] = '\n';
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-j = 0;
-k++;
-i++;
-}
-k++;
-s[k] = '\0';
-return (s);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
 }
