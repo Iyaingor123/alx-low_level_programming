37637dd6098213b5f1b367657bcd47d39561d7ca
iyaingor123 <pri_dekk2001@yahoo.com>
2023-12-18 14:18:37 -0800

    read

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
